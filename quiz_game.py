fb414514b0fc91d279f1e6dae57da843387bbf0f
Lavanya08M <Lavanya08M@github.com>
2024-12-29 12:13:45 -0500

    added comments for first half program

--- quiz_game.py
+++ quiz_game.py
@@ -1,13 +1,18 @@
 def main():
+    # Print welcome statement
     print("\"Welcome to My Computer Quiz!\"")
 
+    # Ask the user whether he/she wants to play the game or not
     playing = input("Do you want to play? ").lower()
 
+    # Check if the playing variable is yes or y or else quit the game
     if playing not in ["yes","y"]:
         quit()
 
+    # If player says yes then start the quiz game
     print("Okay! Lets play :)")
 
+    # Store questions and answers in a dictionaty
     quiz_dict = {
         "CPU": "central processing unit",
         "E-mail": "electronic mail",
@@ -21,16 +26,22 @@ def main():
         "MS": "microsoft"
     }
 
+    # Ask the user how many questions he want to answer in quiz
     number_of_questions = int(input("How many questions do you want to answer (there are 10 queztions)? "))
 
+    # Give a note about points he/she will get for correct and incoorect answers
     print("\n\"Note: You will get 5 points for each correct answer and -2 points for each incorrect answer.\"\n")
 
+    # Store total score of the player in the total_score variable
     total_score = 0
+
+    # count of correct and incorrect answers
     correct_count = 0
     incorrect_count = 0
     ques_no = 1
 
-    print(f"Answer {number_of_questions} quiz questions!")
+
+    print(f"Answer below {number_of_questions} questions in Quiz!\n")
 
  
     for ques, ans in quiz_dict.items():
